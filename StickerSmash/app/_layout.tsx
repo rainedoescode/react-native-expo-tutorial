f84d6c3200440fe54f0c8a0a6b77d0d3218f6b4b
Raine Adams <rainedoescode@gmail.com>
2026-01-11 12:20:27 -0600

    Implement basic in-app routing with about screen

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import {SafeAreaView} from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack>
+        <Stack.Screen name="index" options={{
+          headerTitle: "StickerSmash"
+        }}/>
+        <Stack.Screen name="about" options={{
+          headerTitle: "About",
+        }}/>
+      </Stack>
+  )
 }
